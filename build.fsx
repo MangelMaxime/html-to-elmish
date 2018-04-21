fb0edba17f816ef8b3d8c53857293e11059c4954
Maxime Mangel <maxime.shadaen@protonmail.com>
2018-04-21 21:41:46 +0200

    Release Fable.HtmlConverter package

--- build.fsx
+++ build.fsx
@@ -12,10 +12,22 @@ open Fake.IO.Globbing.Operators
 open Fake.IO.FileSystemOperators
 open Fake.JavaScript
 
-#if MONO
-// prevent incorrect output encoding (e.g. https://github.com/fsharp/FAKE/issues/1196)
-System.Console.OutputEncoding <- System.Text.Encoding.UTF8
-#endif
+module Util =
+
+    let visitFile (visitor: string->string) (fileName : string) =
+        File.ReadAllLines(fileName)
+        |> Array.map (visitor)
+        |> fun lines -> File.WriteAllLines(fileName, lines)
+
+    let replaceLines (replacer: string->Match->string option) (reg: Regex) (fileName: string) =
+        fileName |> visitFile (fun line ->
+            let m = reg.Match(line)
+            if not m.Success
+            then line
+            else
+                match replacer line m with
+                | None -> line
+                | Some newLine -> newLine)
 
 let jsLibsOutput = "src" </> "WebApp" </> "public" </> "libs"
 let testsDist = "tests" </> "dist"
@@ -93,6 +105,68 @@ Target.create "Release" (fun _ ->
     Yarn.exec "run gh-pages --dist src/WebApp/output" id
 )
 
+// --------------------------------------------------------------------------------------
+// Build a NuGet package
+let needsPublishing (versionRegex: Regex) (releaseNotes: ReleaseNotes.ReleaseNotes) projFile =
+    printfn "Project: %s" projFile
+    if releaseNotes.NugetVersion.ToUpper().EndsWith("NEXT")
+    then
+        printfn "Version in Release Notes ends with NEXT, don't publish yet."
+        false
+    else
+        File.ReadLines(projFile)
+        |> Seq.tryPick (fun line ->
+            let m = versionRegex.Match(line)
+            if m.Success then Some m else None)
+        |> function
+            | None -> failwith "Couldn't find version in project file"
+            | Some m ->
+                let sameVersion = m.Groups.[1].Value = releaseNotes.NugetVersion
+                if sameVersion then
+                    printfn "Already version %s, no need to publish." releaseNotes.NugetVersion
+                not sameVersion
+
+let toPackageReleaseNotes (notes: string list) =
+    "* " + String.Join("\n * ", notes)
+    |> (fun txt -> txt.Replace("\"", "\\\""))
+
+let pushNuget (releaseNotes: ReleaseNotes.ReleaseNotes) (projFile: string) =
+    let versionRegex = Regex("<Version>(.*?)</Version>", RegexOptions.IgnoreCase)
+
+    if needsPublishing versionRegex releaseNotes projFile then
+        let projDir = Path.GetDirectoryName(projFile)
+        let nugetKey =
+            match Environment.environVarOrNone "NUGET_KEY" with
+            | Some nugetKey -> nugetKey
+            | None -> failwith "The Nuget API key must be set in a NUGET_KEY environmental variable"
+        (versionRegex, projFile)
+        ||> Util.replaceLines (fun line _ ->
+                                    versionRegex.Replace(line, "<Version>"+releaseNotes.NugetVersion+"</Version>") |> Some)
+
+        let result =
+            DotNet.exec
+                (DotNet.Options.withWorkingDirectory projDir)
+                "pack"
+                (sprintf "-c Release /p:PackageReleaseNotes=\"%s\"" (toPackageReleaseNotes releaseNotes.Notes))
+
+        if not result.OK then failwithf "dotnet fable failed with code %i" result.ExitCode
+
+        Directory.GetFiles(projDir </> "bin" </> "Release", "*.nupkg")
+        |> Array.find (fun nupkg -> nupkg.Contains(releaseNotes.NugetVersion))
+        |> (fun nupkg ->
+            Paket.push (fun p -> { p with ApiKey = nugetKey
+                                          WorkingDir = Path.getDirectory nupkg }))
+
+Target.create "PublishNugets" (fun _ ->
+    !! "./src/HtmlConverter/Fable.HtmlConverter.fsproj"
+    |> Seq.iter(fun s ->
+        let projFile = s
+        let projDir = IO.Path.GetDirectoryName(projFile)
+        let release = projDir </> "RELEASE_NOTES.md" |> ReleaseNotes.load
+        pushNuget release projFile
+    )
+)
+
 Target.create "Setup" Target.DoNothing
 
 Target.create "CI" Target.DoNothing
@@ -124,6 +198,9 @@ Target.create "CI" Target.DoNothing
 
 "Build"
     ==> "Release"
+
+"Restore"
+    ==> "PublishNugets"
 
 "CI"
     <== [ "Build"
