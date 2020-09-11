c60955f4da05417fef205133be3db61077229f64
BooksBaum <15612932+Booksbaum@users.noreply.github.com>
2020-09-11 10:57:49 +0200

    Update Fable
    Note: doesn't compile yet: code isn't adjusted to new fable

--- src/WebApp/webpack.config.js
+++ src/WebApp/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const webpack = require("webpack");
-const fableUtils = require("fable-utils");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
